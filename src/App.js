c629386f50eb5ca7a820a3627e4c76f6fa414cd9
Arthur Fernandes <arthur0139@gmail.com>
2021-12-17 23:03:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,86 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
+import P from 'prop-types';
 
-const eventFn = () => {
-  console.log('função update');
+const Button = ({ incrementButton }) => {
+  console.log('Filho renderizou');
+  return <button onClick={() => incrementButton(10)}>+</button>;
+};
+
+Button.propTypes = {
+  incrementButton: P.func,
 };
 
 function App() {
-  const [reverse, setReverse] = useState(false);
-  const [counter, setcounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  const [counter, setCounter] = useState(0);
 
-  const handleReverseClick = () => {
-    console.log('Reverse click function rederizou!');
-    setReverse(!reverse);
-  };
-
-  const handleCounterClick = ({ num }) => {
-    console.log('Counter click function rederizou!');
-    setcounter((previousCounter) => previousCounter + num);
-  };
-
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    // equivalente a um componenteDidUnmount (evita problemas cm o reload da página)
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  });
-
-  useEffect(() => {
-    console.log('componentDidMount');
+  const incrementCounter = useCallback((num) => {
+    setCounter((counter) => counter + num);
   }, []);
 
-  useEffect(() => {
-    console.log('Dependência atualizou', counter);
-  }, [counter]);
+  const btn = useMemo(() => <Button incrementButton={incrementCounter} />, [incrementCounter]);
+  console.log('Pai renderizou');
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>coiso</h1>
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-        <button type="button" onClick={() => handleReverseClick()}>
-          Reverse {reverseClass}
-        </button>
-
-        <button type="button" onClick={() => handleCounterClick(10)}>
-          Counter {counter}
-        </button>
-      </header>
+      <p>Teste</p>
+      <h1>C1: {counter}</h1>
+      {btn}
     </div>
   );
 }
-/*
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      reverse: false,
-    };
-  }
-
-  render() {
-    const { reverse } = this.state;
-    const reverseClass = reverse ? 'reverse' : '';
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-
-          <button type="button" onClick={() => this.setState({ reverse: true })}>
-            Reverse {reverseClass}
-          </button>
-        </header>
-      </div>
-    );
-  }
-}
-*/
-
 export default App;
